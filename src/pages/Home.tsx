f95a274b52b1c718c4138ebc501863fa0ee6693f
WH-Yoshi <absluca419@gmail.com>
2025-05-09 12:26:42 +0200

    starting home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,39 @@ function Home() {
 
     return (
         <section id="home">
-            <section className="left">
-                <p></p>
-            </section>
-            <section className="right">
-                <article className="top">
-                    <p></p>
-                </article>
-                <button type="button">Prioriser</button>
+            <h1>Priorisez vos rénovations, maximisez vos économies.</h1>
+            <section className="pub">
+                <section className="left">
+                    <article className="text-bubble">
+                        <p>
+                            En Belgique, 50 % des propriétaires envisagent de rénover leur habitation d’ici 2028. Pourtant, <b>67 % des Belges ignorent l’existence des aides publiques</b> et que 28 % seulement en bénéficient lors de leurs travaux. Ces aides peuvent pourtant alléger considérablement le coût des rénovations et accélérer l’atteinte des standards énergétiques, en particulier dans un contexte où 46 % des rénovations dépassent les 50 000 € et où les pénalités d’habitation énergivore arrivent vite.
+                            <br/><br/>
+                            Notre outil vous aide à identifier les rénovations prioritaires, maximiser l’impact sur votre performance énergétique, et accéder aux aides publiques auxquelles vous avez droit.
+                            <br/><br/>
+                            Plus qu’une simple planification, c’est une optimisation complète de votre projet, pensée pour notre planète, le confort, l’économie d’énergie et la valorisation de votre bien immobilier.
+                            <br/><br/>
+                            Ne laissez pas les subventions vous échapper, planifiez intelligemment, économisez durablement.
+                        </p>
+                    </article>
+                </section>
+                <section className="right">
+                    <article className="top">
+                        <p>
+                            Pour tirer le meilleur parti de notre solution, voici ce dont vous aurez besoin :
+                            <br/><br/>
+                            - Un relevé PEB (format pdf) : ce document permettra de définir précisément l’état énergétique de votre habitation.
+                            <br/>
+                            - Informations sur le logement : surface habitable, nombre de pièces, type de chauffage, isolation actuelle, etc.
+                            <br/>
+                            - Objectif de rénovation : améliorer le confort, réduire les factures, augmenter la valeur du bien, ou tout cela à la fois.
+                            <br/>
+                            - Budget estimatif : un montant à investir pour orienter les choix de priorisation.- Localisation du bien : certaines primes dépendent de la région et de la commune.
+                            <br/><br/>
+                            💡 Saviez-vous que 50 % des Belges estiment que le soutien financier devrait venir des pouvoirs publics ?Notre outil vous permet d’identifier ces subventions rapidement et de les intégrer dans votre plan de rénovation priorisé.
+                        </p>
+                    </article>
+                    <button type="button">Prioriser</button>
+                </section>
             </section>
         </section>
     )
--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,5 +1,4 @@
 import Header from "./Header.tsx";
-import Footer from "./Footer.tsx";
 import {Outlet} from "react-router";
 
 function Root() {
@@ -8,7 +7,6 @@ function Root() {
         <>
             <Header />
             <Outlet />
-            <Footer />
         </>
     )
 }
