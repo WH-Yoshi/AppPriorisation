84b5abeb70cdd08914f81b4863dae73a427544be
WH-Yoshi <absluca419@gmail.com>
2025-05-12 21:34:13 +0200

    starting register page

--- src/pages/CreationProfil.tsx
+++ src/pages/CreationProfil.tsx
@@ -23,7 +23,7 @@ export default function CreationProfil() {
         setLoading(true);
         console.log(selectedOption);
         try {
-            await axios.post("https://apppriorisation-api-production.up.railway.app/creation-profil", {
+            await axios.post("https://apppriorisation-api-production.up.railway.app/profil-utilisateur", {
                 objectif_principal: selectedOption,
             });
             alert("Données envoyées avec succès !");
