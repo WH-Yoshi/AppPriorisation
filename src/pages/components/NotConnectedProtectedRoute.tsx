64dd4762698be1e9fbbb0e349d08df71f3c757b6
WH-Yoshi <absluca419@gmail.com>
2025-05-15 00:17:58 +0200

    starting register page

--- src/pages/components/NotConnectedProtectedRoute.tsx
+++ src/pages/components/NotConnectedProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router";
 import {JSX} from "react";
 
 const NonConnectedPrivateRoute = ({ children }: { children: JSX.Element }) => {
-    const token = localStorage.getItem("token");
+    const token = sessionStorage.getItem("token");
     return token ? <Navigate to="/dashboard" /> : children;
 };
 
